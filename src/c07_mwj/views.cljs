07ca09a1e2aac8e0688e270a2f572779dea3279f
kees- <6820950+kees-@users.noreply.github.com>
2022-03-29 17:28:50 -0700

    Add a piece of content

--- src/c07_mwj/views.cljs
+++ src/c07_mwj/views.cljs
@@ -3,6 +3,11 @@
    [re-frame.core :as re-frame]
    [c07-mwj.subs :as subs]))
 
+(defn citrus []
+  [:div.citrus-cage
+   [:span "My citrus in bloom"]])
+
 (defn main-panel []
   (let []
-    [:div]))
+    [:div
+     [citrus]]))
