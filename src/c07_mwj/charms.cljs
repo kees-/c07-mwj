9482041ea3f8785b16494456b800f5218bf2fdd6
kees- <6820950+kees-@users.noreply.github.com>
2022-04-11 16:49:11 -0700

    Refactor, docstrings, and notes

--- src/c07_mwj/charms.cljs
+++ src/c07_mwj/charms.cljs
@@ -7,49 +7,76 @@
    ["contactjs" :as contact]))
 
 (defn add-props
+  "Add an empty parameter map to hiccup component if not present."
   [hiccup]
   (if (map? (hiccup 1))
     hiccup
     (reduce into [[(hiccup 0)] [{}] (rest hiccup)])))
 
+;; NOTES
+;  Set the ID for the charm in the options map (:id), not in the component.
+;
+;  The mindset here is that you can theoretically pass any component to the
+;  charmer. As long as its children are compatible with the movement gestures,
+;  most HTML should be safe. The status of form components are unknown.
 (defn charm
-  [{:keys [id]
-    :or {}}
-   content]
+  "Return a generic charm element. Pass a map of params and a hiccup form."
+  [{:keys [id] :or {}} content]
   (reagent/create-class
    {:reagent-render
     (fn []
       (-> content
+          ; Add property map if missing
           add-props
+          ; Set the ID of the component
           (assoc-in [1 :id] id)
+          ; Append necessary classes for charm display
           (update-in [1 :class] str " charm no-select hidden")))
     :component-did-mount
     (fn []
-      (let [zero (fn [v] (if (= "" v) 0 (js/parseFloat v)))
-            el (js/document.getElementById id)
-            opts (clj->js {:DEBUG false})
-            position (fn [element x2 y2]
-                       (set! (.. element -style -left)
-                         (-> (.. element -style -left) zero (+ x2) (str "px")))
-                       (set! (.. element -style -top)
-                         (-> (.. element -style -top) zero (+ y2) (str "px"))))
-            translate (fn [element x y]
-                        (set! (.. element -style -transform)
-                          (str "translate(" x "px, " y "px)")))]
-        (set! (.. el -style -left)
-          (-> el .-offsetWidth logic/spawn-x (str "px")))
-        (set! (.. el -style -top)
-          (-> el .-offsetHeight logic/spawn-y (str "px")))
-        (-> el .-classList (.remove "hidden"))
-        (contact/PointerListener. el opts)
-        (.addEventListener el "pan"
+      (let
+        [; Avoid NaN error preventing addition. Empty string equivalent to 0
+         nan-zero (fn [v] (if (= "" v) 0 (js/parseFloat v)))
+         ; Hacky `this` ?
+         me (js/document.getElementById id)
+         ; A set of options for the element's gesture listener
+         ; https://biodiv.github.io/contactjs/documentation/contact-js/#Options
+         opts #js{:DEBUG false}
+         ; Add X and Y to a given element's current LEFT and TOP properties
+         position (fn [el x y]
+                    (oset! el "style.left"
+                      (-> el (oget "style.left") nan-zero (+ x) (str "px")))
+                    (oset! el "style.top"
+                      (-> el (oget "style.top") nan-zero (+ y) (str "px"))))
+         ; Directly set the X and Y value of an element's transform property
+         translate (fn [el x y]
+                     (oset! el "style.transform"
+                       (str "translate(" x "px, " y "px)")))]
+        ; Spawn the charm at a random point
+        ; The element width is used to calculate not spawning near the edge
+        ; Upcoming, a coord option to override random spawning
+        (oset! me "style.left"
+          (-> me (oget "offsetWidth") logic/spawn-x (str "px")))
+        (oset! me "style.top"
+          (-> me (oget "offsetHeight") logic/spawn-y (str "px")))
+        ; Reveal the charm in the rendered DOM
+        (-> me (oget "classList") (.remove "hidden"))
+        ; Create the all-important all-hearing Contact.js listener
+        (contact/PointerListener. me opts)
+        ; Function which fires WHILE DRAGGING a charm
+        (.addEventListener me "pan"
          (fn [e]
-           (translate el
-                 (.. e -detail -global -deltaX)
-                 (.. e -detail -global -deltaY))))
-        (.addEventListener el "panend"
+           (translate me
+             (oget e "detail.global.deltaX")
+             (oget e "detail.global.deltaY"))))
+        ; Function which fires when user RELEASES charm
+        ; Ending a pan 'locks in' the (x,y) offset of the gesture
+        ; The next pan will begin with a delta of (0, 0), so set that now
+        (.addEventListener me "panend"
          (fn [e]
-           (translate el 0 0)
-           (position el
-                     (.. e -detail -global -deltaX)
-                     (.. e -detail -global -deltaY))))))}))
+           ; Update the element's base position
+           (position me
+             (oget e "detail.global.deltaX")
+             (oget e "detail.global.deltaY"))
+           ; Revert the transform to original position
+           (translate me 0 0)))))}))
