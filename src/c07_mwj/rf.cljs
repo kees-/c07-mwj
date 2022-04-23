5154891e29aa71392e58f02d481fd0f1d7efaedd
kees- <6820950+kees-@users.noreply.github.com>
2022-04-23 11:29:37 -0700

    Add nominal sample events/subs

--- src/c07_mwj/rf.cljs
+++ src/c07_mwj/rf.cljs
@@ -1,19 +1,31 @@
 (ns c07-mwj.rf
   (:require
-   [re-frame.core :as re-frame]))
+   [re-frame.core :as re-frame
+    :refer [reg-event-db reg-event-fx reg-sub]]))
 
+;; ========== INITIAL SETUP ====================================================
 (def default-db
-  {})
+  {:nav
+   {:charm-depth 0}})
 
 (def >evt re-frame/dispatch)
 (def <sub (comp deref re-frame/subscribe))
 
-(re-frame/reg-event-db
+;; ========== EVENTS ===========================================================
+(reg-event-db
  ::initialize-db
  (fn [_ _]
    default-db))
 
-; (re-frame/reg-sub
-;  ::name
-;  (fn [db]
-;    (:name db)))
+(reg-event-db
+ ::inc-depth
+ (fn [db _]
+   (update-in db [:nav :charm-depth] inc)))
+
+(reg-event-db
+ ::dec-depth
+ (fn [db _]
+   (update-in db [:nav :charm-depth] dec)))
+
+;; ========== SUBS =============================================================
+(reg-sub ::depth #(-> % :nav :charm-depth))
