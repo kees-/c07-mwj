a55143f2cd62d3d5a8ff500021392e43525d326c
kees- <6820950+kees-@users.noreply.github.com>
2022-04-23 09:51:47 -0700

    Update charms & styles

--- src/c07_mwj/views.cljs
+++ src/c07_mwj/views.cljs
@@ -2,6 +2,7 @@
   (:require
    [c07-mwj.charms :as charms]
    [c07-mwj.logic :as logic]
+   [c07-mwj.assets :as assets]
    [c07-mwj.utilities :as util]
    [c07-mwj.debug :as debug]))
 
@@ -19,13 +20,22 @@
 
 (defn main-panel []
   (let []
-    [:div
+    [:<>
      [logo]
      [charms/charm
       {:id "citrus-cage"}
+      [:div.koi-2
+       [:svg
+        {:viewBox "0 0 24 24"
+         :width "156"
+         :style {:fill "lemonchiffon"}}
+        [:path {:d assets/mail-svg}]]
+       [:span "Mailer"]]]
+     [charms/charm
+      {:id "newt"}
       [:div.koi-1
-       [:span "Now in bloom"]
-       [:article.glyph (str (char 4305))]]]
+       [:span "Now this"]
+       [:article.glyph "2"]]]
      ; DEBUG
      #_[debug/pointer-elements]
      #_[debug/xy-elements]
