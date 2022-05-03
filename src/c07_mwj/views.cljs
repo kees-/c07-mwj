973bae625f114b92ef89554b57a8aba92906a878
kees- <6820950+kees-@users.noreply.github.com>
2022-05-03 12:08:16 -0700

    Add some new content inline

--- src/c07_mwj/views.cljs
+++ src/c07_mwj/views.cljs
@@ -3,7 +3,7 @@
    [c07-mwj.charms :as charms]
    [c07-mwj.logic :as logic]
    [c07-mwj.assets :as assets]
-   [c07-mwj.utilities :as util]
+   ; [c07-mwj.utilities :as util]
    [c07-mwj.debug :as debug]))
 
 ;; ========== CONTENTS =========================================================
@@ -20,42 +20,67 @@
      [:small>article#copyright (char 169) " " owner " " (logic/year)]]))
 
 ;; ========== CHARMS ===========================================================
-(defn mailer
-  []
-  [charms/charm
-   {:id "mailer"
-    :container
-    [:div.koi-2
-     [:svg
-      {:viewBox "0 0 24 24"
-       :width "156"
-       :style {:fill "lemonchiffon"}}
-      [:path {:d assets/mail-svg}]]
-     [:span "Mailer"]]
-    :contains
-    [[:div
-      {:style {:display "block"
-               :margin "0 auto"
-               :width "max-content"}}
-      [util/form-loader "5fd5468ccdc040ff3b12248e"]]]}])
+; (defn mailer
+;   []
+;   [charms/charm
+;    {:id "mailer"
+;     :container
+;     [:div.koi-2
+;      [:svg
+;       {:viewBox "0 0 24 24"
+;        :width "156"
+;        :style {:fill "lemonchiffon"}}
+;       [:path {:d assets/mail-svg}]]
+;      [:span "Mailer"]]
+;     :contains
+;     [[:div
+;       {:style {:display "block"
+;                :margin "0 auto"
+;                :width "max-content"}}
+;       [util/form-loader "5fd5468ccdc040ff3b12248e"]]]}])
 
-(defn newt
-  []
-  [charms/charm
-   {:id "newt"
-    :container
-    [:div.koi-1
-     [:span "Now this"]
-     [:article.glyph "0"]]}])
+; (defn newt
+;   []
+;   [charms/charm
+;    {:id "newt"
+;     :container
+;     [:div.koi-1
+;      [:span "Now this"]
+;      [:article.glyph "0"]]}])
 
-(defn citrus
+; (defn citrus
+;   []
+;   [charms/charm
+;    {:id "citrus"
+;     :container
+;     [:div.koi-1
+;      [:article.glyph (char 4305)]
+;      [:span "Now in bloom"]]}])
+
+(defn bottle
   []
   [charms/charm
-   {:id "citrus"
+   {:id "bottle"
     :container
-    [:div.koi-1
-     [:article.glyph (char 4305)]
-     [:span "Now in bloom"]]}])
+    [:div.koi-3
+     [:div
+      [:img {:src "/_asset/bottle.svg"
+             :alt "A couplet: A well contained emptiness, empty kept, your beautiful bottle, shared from the top."
+             :draggable false
+             :width 180
+             :height 265}]]]}])
+
+(defn mail
+  []
+  [charms/charm
+   {:id "mail"
+    :container
+    [:div.koi-4
+     [:img {:src "/_asset/mail.gif"
+            :alt "A gif: You'll Get Mail"
+            :draggable false
+            :width 54
+            :height 63}]]}])
 
 ;; ========== PANELS ===========================================================
 (defn main-panel []
@@ -63,16 +88,10 @@
    [logo]
    ; [mailer]
    ; [newt]
-   [citrus]
+   ; [citrus]
+   [bottle]
+   [mail]
    ; DEBUG
    #_[debug/pointer-elements]
    #_[debug/xy-elements]
    [footer]])
-
-(comment
- [:div
-  {:id "flodesk"
-   :style {:display "block"
-           :margin "0 auto"
-           :width "max-content"}}
-  [util/form-loader "5fd5468ccdc040ff3b12248e"]])
