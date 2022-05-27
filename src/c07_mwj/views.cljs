3049342babadc3a7f0fa214289ab58edd5cd8c9c
kees- <6820950+kees-@users.noreply.github.com>
2022-05-27 08:12:54 -0700

    Swap out elements: sinking leaves

--- src/c07_mwj/views.cljs
+++ src/c07_mwj/views.cljs
@@ -57,18 +57,18 @@
 ;      [:article.glyph (char 4305)]
 ;      [:span "Now in bloom"]]}])
 
-(defn bottle
-  []
-  [charms/charm
-   {:id "bottle"
-    :container
-    [:div.koi-3
-     [:div
-      [:img {:src "/_asset/bottle.svg"
-             :alt "A couplet: A well contained emptiness, empty kept, your beautiful bottle, shared from the top."
-             :draggable false
-             :width 180
-             :height 265}]]]}])
+; (defn bottle
+;   []
+;   [charms/charm
+;    {:id "bottle"
+;     :container
+;     [:div.koi-3
+;      [:div
+;       [:img {:src "/_asset/bottle.svg"
+;              :alt "A couplet: A well contained emptiness, empty kept, your beautiful bottle, shared from the top."
+;              :draggable false
+;              :width 180
+;              :height 265}]]]}])
 
 (defn mail
   []
@@ -82,19 +82,82 @@
             :width 54
             :height 63}]]}])
 
-(defn bloom
+; (defn bloom
+;   []
+;   [charms/charm
+;    {:id "blue"
+;     :container
+;     [:div.koi-5
+;      [:span "Settling"]
+;      [:div.trigram.trigram2
+;       [:table>tbody
+;        [:tr [:td] [:td.split] [:td]]
+;        [:tr [:td] [:td] [:td]]
+;        [:tr [:td] [:td.split] [:td]]
+;        [:tr [:td {:colspan 3}]]]]]}])
+
+(defn leaf-1
   []
   [charms/charm
-   {:id "blue"
+   {:id "leaves"
     :container
-    [:div.koi-5
-     [:span "Blooming"]
-     [:div.trigram
-      [:table>tbody
-       [:tr [:td] [:td.split] [:td]]
-       [:tr [:td] [:td] [:td]]
-       [:tr [:td] [:td] [:td]]
-       [:tr [:td {:colspan 3}]]]]]}])
+    [:div
+     {:style {:padding "1rem .5rem"
+              :background "black"
+              :border "2px solid #262626"}}
+     [:div
+      {:style {:padding ".5rem"
+               :background "#1a1a1a"
+               :border "2px groove #4d4d4d"}}
+      [:div
+       {:style {:padding ".2rem"
+                :background "#262626"
+                :border "1px groove silver"}}
+       [:video
+        {:src "/_asset/leaf-1.webm"
+         :type "video/webm"
+         :width 135
+         :height 135
+         :autoplay ""
+         :loop "loop"
+         :muted "muted"
+         :playsinline ""}]]]]}])
+
+(defn leaf-2
+  []
+  [charms/charm
+   {:id "leaf-2"
+    :container
+    [:div
+     {:style {:border "2px outset #004D4D"
+              :border-radius 1
+              :padding ".4rem"
+              :background "#033"}}
+     [:div
+      {:style {:border "3px inset teal"
+               :padding ".25rem"
+               :background "lightgrey"}}
+      [:img
+       {:src "/_asset/leaf-2.png"
+        :height 60}]]]}])
+
+(defn leaf-3
+  []
+  [charms/charm
+   {:id "leaf-2"
+    :container
+    [:div
+     {:style {:border "2px outset sienna"
+              :border-radius 1
+              :padding ".4rem"
+              :background "#c9641d"}}
+     [:div
+      {:style {:border "3px inset darksalmon"
+               :padding ".25rem"
+               :background "wheat"}}
+      [:img
+       {:src "/_asset/leaf-3.png"
+        :width 50}]]]}])
 
 ;; ========== PANELS ===========================================================
 (defn main-panel []
@@ -103,9 +166,12 @@
    ; [mailer]
    ; [newt]
    ; [citrus]
-   [bottle]
+   ; [bottle]
+   ; [bloom]
    [mail]
-   [bloom]
+   [leaf-1]
+   [leaf-2]
+   [leaf-3]
    ; DEBUG
    #_[debug/pointer-elements]
    #_[debug/xy-elements]
